b482dc5ebfaa7589795e0ffc5115e9098ee240c5
Michael Xie <xie.michael.x@gmail.com>
2024-11-05 23:24:46 +1100

    delete obsolete drawarrow.zig

--- src/drawArrow.zig
+++
@@ -1,7 +0,0 @@
-const rl = @import("raylib");
-// Load assets for single arrow to copy from
-
-fn applyNoteConstantAlpha(note: *rl.Image, alpha: u32) *rl.Image {
-    const mask = rl.genImageColor(note.width, note.height, rl.Color(0,0,0,alpha));
-    return rl.imageAlphaMask(note, mask);
-}
